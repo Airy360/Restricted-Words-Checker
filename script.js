9193c97a4d80a2db9c2f9922f303eaeb5883bbe5
Airy360 <mubashirfazal327@gmail.com>
2024-12-23 17:56:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const restrictedKeywords = [
     "alopecia", "ageing", "aging", "testing", "toxins", "toxin", "anti-bacterial", "germs", "bacterial",
     "illness", "safest", "antibiotics", "antibiotic", "healing", "eczema", "anti-biotic", "anti-biotics",
     "100% Cotton", "100% Pure Cotton", "GMP-Verified", "fight", "combat", "aromatherapy", "therapeutic",
+    "migraine", "cervical", "insomnia", "herniated disc", "arthritis", "apnea",
 ];
 
 const suggestionsMap = {
